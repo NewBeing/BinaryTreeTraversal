a017a51906baa6d8fc393d4dd777ccb42e10603b
JunQiao <MegJoe@163.com>
2018-07-10 21:54:00 +0800

    Update BinaryTreeTraversal.cpp

--- BinaryTreeTraversal.cpp
+++ BinaryTreeTraversal.cpp
@@ -70,3 +70,42 @@ public:
         return result;
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> result;
+        stack<TreeNode*> s;
+        TreeNode * p = root;
+        TreeNode* q;  //记录刚刚访问过的节点
+        do{
+            while (p != NULL) {
+                s.push(p);
+                p = p->left;
+            }
+            q = NULL;
+            while (!s.empty()) {
+                p = s.top();
+                s.pop();
+                if (p->right == q) {  //当右子树已经访问过了，才可以访问根
+                    result.push_back(p->val);
+                    q = p;  //记录刚刚访问过的节点
+                } else {
+                    s.push(p); //第一次访问到该节点，需要将它重新入栈
+                    p = p->right;
+                    break;
+                }
+            }
+        } while (!s.empty());
+        return result;
+    }
+};
